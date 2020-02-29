2819892829e003fdaf62bdbdc807dff50f70aa1b
papat27 <pqpagba@gmail.com>
2020-02-29 14:59:36 -0500

    implemented get answers

--- server/controllers/answerController.js
+++ server/controllers/answerController.js
@@ -1,19 +1,65 @@
 const answerModel = require('../models/answerModel.js');
 const photoModel = require('../models/photoModel.js');
 
+// {
+//   "question": "1",
+//   "page": 0,
+//   "count": 5,
+//   "results": [
+//     {
+//       "answer_id": 8,
+//       "body": "What a great question!",
+//       "date": "2018-01-04T00:00:00.000Z",
+//       "answerer_name": "metslover",
+//       "helpfulness": 8,
+//       "photos": [],
+//     },
+//     {
+//       "answer_id": 5,
+//       "body": "Something pretty durable but I can't be sure",
+//       "date": "2018-01-04T00:00:00.000Z",
+//       "answerer_name": "metslover",
+//       "helpfulness": 5,
+//       "photos": [{
+//           "id": 1,
+//           "url": "urlplaceholder/answer_5_photo_number_1.jpg"
+//         },
+//         {
+//           "id": 2,
+//           "url": "urlplaceholder/answer_5_photo_number_2.jpg"
+//         },
+//         // ...
+//       ]
+//     },
+//     // ...
+//   ]
+// }
+
 module.exports = {
-  getAnswers: async (req, res) => {
+  getAnswers: (req, res) => {
     let page = req.query.page ? req.query.page : 1;
     let count = req.query.count ? req.query.count : 5;
+    let result = {
+      question: req.params.question_id,
+      page: page,
+      count: count
+    };
 
-    let data = await answerModel.read(req.params, page, count);
-
-    res.send(data);
-
-    // answerModel.read(req.params, page, count)
-    //   .then((data) => {
-    //     res.send('answer get');
-    //   });
+    answerModel.read(req.params, page, count)
+      .then((data) => {
+        data.forEach((answer) => {
+          answer.photos.forEach((photo) => {
+            delete photo._id;
+            delete photo.answer_id;
+          })
+        })
+        result.results = data;
+        res.send(result);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(404);
+      });
   },
 
   addAnswer: (req, res) => {
--- server/models/answerModel.js
+++ server/models/answerModel.js
@@ -1,4 +1,4 @@
-const db = require('../../db/index.js');
+const {Answer} = require('../../db/index.js');
 
 // {
 //   "question": "1",
@@ -34,69 +34,11 @@ const db = require('../../db/index.js');
 //   ]
 // }
 
-// SELECT json_agg(p) FROM (SELECT id, url from photos WHERE answer_id = 5) p
-// SELECT a.id, a.body, a.answer_date AS date, a.answerer_name, a.helpfulness, (SELECT json_agg(p) FROM (SELECT id, url from photos WHERE answer_id = a.id) p) AS photos FROM answers AS a WHERE a.question_id = 1 ORDER BY a.helpfulness DESC LIMIT 5 OFFSET 0
-
-// SELECT row_to_json(ans) AS answers
-// FROM (
-//   SELECT a.id, a.body, a.answer_date AS date, a.answerer_name, a.helpfulness,
-//   (SELECT json_agg(p)
-//     FROM (
-//       SELECT id, url from photos WHERE answer_id = a.id
-//     ) p
-//   ) AS photos
-// FROM answers AS a
-// WHERE a.question_id = ${question_id}
-// ORDER BY a.helpfulness DESC
-// LIMIT ${count}
-// OFFSET ${offset}) ans
-
-// SELECT answer_id, body, answer_date as date, answerer_name, helpfulness,
-//   (
-//     SELECT array_to_json(coalesce(array_agg(row_to_json(d)),'{}'))
-//     FROM (
-//       SELECT id, url
-//       FROM photos
-//       WHERE answer_id=answers.answer_id
-//     ) d
-//   ) AS photos
-// FROM answers
-// WHERE question_id = ${questionId} AND reported = 0 LIMIT ${count} OFFSET ${(page -
-//   1) *
-//   count}`
-
-// SELECT a.id AS answer_id, a.body, a.answer_date as date, a.answerer_name, a.helpfulness, (SELECT array_to_json(coalesce(array_agg(row_to_json(d)),'{}')) FROM (SELECT id, url FROM photos WHERE answer_id = a.id) d) AS photos FROM answers AS a WHERE question_id = 1 AND reported = 0 LIMIT 5 OFFSET 0
-// SELECT a.id AS answer_id, a.body, a.answer_date as date, a.answerer_name, a.helpfulness,
-//       (
-//         SELECT array_to_json(coalesce(array_agg(row_to_json(d)),'{}'))
-//         FROM (
-//           SELECT id, url
-//           FROM photos
-//           WHERE answer_id = a._id
-//         ) d
-//       ) AS photos
-//     FROM answers a
-//     WHERE question_id = ${question_id} AND reported = 0 
-//     LIMIT ${count} 
-//     OFFSET ${offset}
-
-// SELECT id AS answer_id, body, answer_date as date, answerer_name, helpfulness FROM answers WHERE question_id = 1 AND reported = 0 LIMIT 5 OFFSET 0
-
 module.exports = {
-  read: async ({question_id}, page, count) => {
-    let offset = (page - 1) * count;
+  read: ({question_id}, page, count) => {
+    let skip = (page - 1) * count;
 
-    let queryString = `
-    SELECT id AS answer_id, body, answer_date as date, answerer_name, helpfulness
-    FROM answers
-    WHERE question_id = ${question_id} AND reported = 0 
-    LIMIT ${count} 
-    OFFSET ${offset}
-    `;
-
-    let query = await db.query(queryString);
-
-    return query;
+    return Answer.find({question_id: question_id},'answer_id body date answerer_name helpfulness photos -_id').skip(skip).limit(count);
   },
 
   readAll: (question_id) => {
