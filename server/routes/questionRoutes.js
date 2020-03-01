438c717c46f53ba3d1a218752612ce2b2162620e
papat27 <pqpagba@gmail.com>
2020-03-01 16:46:47 -0500

    file clean

--- server/routes/questionRoutes.js
+++
@@ -1,9 +0,0 @@
-const questionRouter = require('express').Router();
-const questionController = require('../controllers/questionController.js');
-
-questionRouter.get('/:product_id', questionController.getQuestions);
-questionRouter.post('/:product_id', questionController.addQuestion);
-questionRouter.put('/question/:question_id/helpful', questionController.markHelpful);
-questionRouter.put('question/:question_id/report', questionController.reportQuestion);
-
-module.exports = questionRouter;
